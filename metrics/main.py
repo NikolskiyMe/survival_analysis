2e7ad986b3fb8c23c394218f2ae4b6546be6bff6
Vladimir Nikolskiy <vladimir.nikolskiy@vkpay.com>
2022-03-05 19:05:52 +0300

    Add property methods score

--- metrics/main.py
+++ metrics/main.py
@@ -11,21 +11,25 @@ from sksurv.metrics import (
     as_integrated_brier_score_scorer
 )
 
-from sklearn.metrics import mean_absolute_error
 
-from utils.errors import ParameterError
-
-
-class ScoreBaseA:
+class ScoreBaseA(ABC):
     def __init__(self, survival_train, survival_test, estimate):
         self.survival_train = survival_train
         self.survival_test = survival_test
         self.estimate = estimate
 
+    @abstractmethod
+    def score(self):
+        pass
 
-class ScoreBaseB:
+
+class ScoreBaseB(ABC):
     def __init__(self, estimator):
         self.estimator = estimator
+
+    @abstractmethod
+    def score(self):
+        pass
 
 
 class BrierScore(ScoreBaseA):
@@ -33,27 +37,61 @@ class BrierScore(ScoreBaseA):
         super().__init__(survival_train, survival_test, estimate)
         self.times = times
 
+    @property
+    def score(self):
+        metric = brier_score(self.survival_train,
+                             self.survival_test,
+                             self.estimate,
+                             self.times)
+        return metric  # times, brier scores
+
 
 class ConcordanceIndexCensored:
-    def __init__(self, event_indicator, event_time, estimate, tied_tol):
+    def __init__(self, event_indicator, event_time, estimate, tied_tol=1e-08):
         self.event_indicator = event_indicator
         self.event_time = event_time
         self.estimate = estimate
         self.tied_tol = tied_tol
 
+    @property
+    def score(self):
+        metric = concordance_index_censored(self.event_indicator,
+                                            self.event_time,
+                                            self.estimate,
+                                            self.tied_tol)
+        return metric  # cindex, concordant, discordant, tied_risk, tied_time
+
 
 class ConcordanceIndexIpcw(ScoreBaseA):
-    def __init__(self, survival_train, survival_test, estimate, tau, tied_tol):
+    def __init__(self, survival_train, survival_test, estimate, tau=None, tied_tol=1e-08):
         super().__init__(survival_train, survival_test, estimate)
         self.tau = tau
         self.tied_tol = tied_tol
 
+    @property
+    def score(self):
+        metric = concordance_index_ipcw(self.survival_train,
+                                        self.survival_test,
+                                        self.estimate,
+                                        self.tau,
+                                        self.tied_tol)
+        return metric
+
 
 class CumulativeDynamicAuc(ScoreBaseA):
-    def __init__(self, survival_train, survival_test, estimate, times, tied_tol):
+    def __init__(self, survival_train, survival_test, estimate, times, tied_tol=1e-08):
         super().__init__(survival_train, survival_test, estimate)
         self.times = times
         self.tied_tol = tied_tol
+
+    @property
+    def score(self):
+        metric = cumulative_dynamic_auc(self.survival_train,
+                                        self.survival_test,
+                                        self.estimate,
+                                        self.times,
+                                        self.tied_tol)
+        return metric
 
 
 class IntegratedBrierScore(ScoreBaseA):
@@ -61,19 +99,41 @@ class IntegratedBrierScore(ScoreBaseA):
         super().__init__(survival_train, survival_test, estimate)
         self.times = times
 
+    @property
+    def score(self):
+        metric = integrated_brier_score(self.survival_train,
+                                        self.survival_test,
+                                        self.estimate,
+                                        self.times)
+        return metric
+
 
 class AsConcordanceIndexIpcwScorer(ScoreBaseB):
-    def __init__(self, estimator, tau, tied_tol):
+    def __init__(self, estimator, tau=None, tied_tol=1e-08):
         super().__init__(estimator)
         self.tau = tau
         self.tied_tol = tied_tol
 
+    @property
+    def score(self):
+        metric = as_concordance_index_ipcw_scorer(self.estimator,
+                                                  self.tau,
+                                                  self.tied_tol)
+        return metric
+
 
 class AsCumulativeDynamicAucScorer(ScoreBaseB):
-    def __init__(self, estimator, times, tied_tol):
+    def __init__(self, estimator, times, tied_tol=1e-08):
         super().__init__(estimator)
         self.times = times
         self.tied_tol = tied_tol
+
+    @property
+    def score(self):
+        metric = as_cumulative_dynamic_auc_scorer(self.estimator,
+                                                  self.times,
+                                                  self.tied_tol)
+        return metric
 
 
 class AsIntegratedBrierScoreScorer(ScoreBaseB):
@@ -81,48 +141,9 @@ class AsIntegratedBrierScoreScorer(ScoreBaseB):
         super().__init__(estimator)
         self.times = times
 
+    @property
+    def score(self):
+        metric = as_integrated_brier_score_scorer(self.estimator,
+                                                  self.times)
+        return metric
 
-"""
-# estimate - оценка
-class Score:
-
-    def __init__(self, estimate=None):
-        self.estimate = estimate
-
-    def drop_estimate(self):
-        self.estimate = None
-
-    # ToDo: как передавать тип оценки
-    def __call__(self, m_type=1, *args):
-        if args == ():
-            raise ParameterError('Недостаточно параметров')
-
-        if self.estimate == 'mae':
-            try:
-                score = mean_absolute_error(y_true=args[0], y_pred=args[1])
-            except BaseException:
-                raise ParameterError('Неверные параметры для mae')
-            else:
-                return score
-
-        if m_type == 1:
-            try:
-                # ToDo: как передавать параметры?
-                score = brier_score(survival_train=args[0],
-                                    survival_test=args[1],
-                                    estimate=self.estimate, times=args[3])
-            except BaseException:  # ToDo: Какое исключение?
-                raise ParameterError('Неверные параметры для brier_score')
-            else:
-                return score
-
-        elif m_type == 2:
-            try:
-                score = concordance_index_censored(event_indicator=args[0],
-                                                   event_time=args[1],
-                                                   estimate=self.estimate)
-            except BaseException:
-                raise ParameterError('Неверные параметры для cindex')
-            else:
-                return score
-"""
